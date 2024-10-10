53f4204c3426efbf7da23263685f8a5c26ad1591
makla12 <144844324+makla12@users.noreply.github.com>
2024-10-10 20:11:30 +0200

    update

---
+++ client/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  	return (
+    	<>
+      		<div className="flex flex-col">
+				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam, labore.</p>
+				<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam, labore.</p>
+			</div>
+    	</>
+  	);
+}
