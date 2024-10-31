b190b9e827cd97564710c7e071caffc9158f6df4
makla12 <1234makla@gmail.com>
2024-10-31 10:31:02 +0100

    update (Fix errors)

---
+++ client/components/cell.jsx
@@ -0,0 +1,28 @@
+import { useSignals } from "@preact/signals-react/runtime";
+import Image from "next/image";
+import flagImg from "@/public/flag.png"
+
+export function Cell({handleDig, handleFlag, sig}){
+    useSignals();
+    console.log(sig);
+    return(
+        <div onClick={ handleDig } 
+            onContextMenu={ handleFlag }
+            className="w-10 h-10 border-[#3a4650] border-[1px] flex items-center justify-center select-none hover:cursor-default"
+        >
+            <div className={
+                `flex items-center justify-center z-10 
+                transition-transform bg-blue-500 w-full h-full 
+                ${(!sig.value.cleared && !sig.value.flaged) ? "hover:bg-blue-900" : ""} 
+                ${sig.value.cleared ? "duration-700 scale-0" : (sig.value.flaged ? "duration-150 scale-[0.8] bg-gray-500" : "scale-100")}`}
+            >
+                {sig.value.flaged ? 
+                    <div className="w-[80%] h-[80%]">
+                        <Image src={flagImg} alt="Flag" draggable={false} />
+                    </div> 
+                : ""}
+            </div>
+            <div className="absolute z-0 text-2xl">{sig.value.minesAround == 0 ? "" : sig.value.minesAround}</div>
+        </div>
+    )
+}
--- client/components/game.jsx
+++ client/components/game.jsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from "react";
-import Image from "next/image";
-import flagImg from "@/public/flag.png"
 import { GameNav } from "./gameNav";
 import { EndScrean } from "./endScrean";
+import { signal, useSignal, useSignalEffect } from "@preact/signals-react";
+import { Cell } from "./cell";
 
 export function Game(props){    
     const [board, setBoard] = useState([]);
     const [change, setChange] = useState(false);
-    const [mines, setMines] = useState(0);
     const [gameState, setGameState] = useState(0);
     const [gameWin, setGameWin] = useState(false);
 
@@ -18,37 +17,20 @@ export function Game(props){
     }
 
     const dig = (digs) => {
-        let newBoard = Array.from(board);
         for(let i = 0; i< digs.length; i++){
-            if(board[digs[i][0]][digs[i][1]].flaged || board[digs[i][0]][digs[i][1]].cleared){
+            if(board[digs[i][0]][digs[i][1]].value.flaged || board[digs[i][0]][digs[i][1]].value.cleared){
                 return 0;
             }
             
-            newBoard[digs[i][0]][digs[i][1]].minesAround = digs[i][2];
-            newBoard[digs[i][0]][digs[i][1]].cleared = true;
+            board[digs[i][0]][digs[i][1]].value.minesAround = digs[i][2];
+            board[digs[i][0]][digs[i][1]].value.cleared = true;
         }
-        setBoard(newBoard);
-        setChange(!change);
     }
 
     const flag = (flags) => {
-        let newBoard = Array.from(board);
-        let flagChange = 0;
         for(let i = 0; i < flags.length; i++){
-            if(board[flags[i][0]][flags[i][1]].cleared){
-                continue;
-            }
-            if(newBoard[flags[i][0]][flags[i][1]].flaged){
-                flagChange--;
-            }
-            else{
-                flagChange++;
-            }
-            newBoard[flags[i][0]][flags[i][1]].flaged = !newBoard[flags[i][0]][flags[i][1]].flaged;
+            board[flags[i][0]][flags[i][1]].value.flaged = !board[flags[i][0]][flags[i][1]].value.flaged;
         }
-        setMines(mines - flagChange);
-        setBoard(newBoard);
-        setChange(!change); 
     }
     
     useEffect(()=>{
@@ -57,15 +39,7 @@ export function Game(props){
             setGameWin(value);
         }
         props.socket.on("endGame", handleGameEnd);
-
-        return () => {
-            props.socket.off("endGame", handleGameEnd);
-        }
     },[]);
-
-    useEffect(()=>{
-        setMines(props.mines);
-    },[props.mines, props.gameReset])
 
     useEffect(()=>{
         setGameState(0);
@@ -73,49 +47,37 @@ export function Game(props){
         for(let i = 0;i < props.rows;i++){
             newBoard.push([]);
             for(let j = 0;j < props.cols; j++){
-                newBoard[i].push({cleared:false, flaged:false,minesAround:0})
+                newBoard[i].push(signal({cleared:false, flaged:false,minesAround:0}))
             }
         }
         setBoard(newBoard);
         setChange(!change);
     },[props.cols,props.rows,props.gameReset]);
-        
+
     useEffect(()=>{
         props.socket.on("dig",dig);
         props.socket.on("flag",flag);
 
-        return ()=>{
+        return () => {
             props.socket.off("dig",dig);
             props.socket.off("flag",flag);
         }
     },[change])
-    
+
     return(
         <>
-        <GameNav mines={mines} socket={props.socket} />
+        
+        <GameNav socket={props.socket} minesStart={props.mines} gameReset={props.gameReset} board={board} />
         <div className="min-w-screen min-h-screen w-fit h-fit flex justify-center items-center p-24 mx-auto">
             <div className="flex flex-col">
-                {board.map((value1,index1)=>(
+                {board.map((row,index1)=>(
                     <div key={index1} className="flex">
-                        {value1.map((value2,index2)=>(
-                            <div key={`${index1} ${index2}`} onClick={()=>{ props.socket.emit("dig", props.roomId, index1, index2) }} 
-                                onContextMenu={(e)=>{e.preventDefault(); props.socket.emit("flag", props.roomId, index1, index2) }}
-                                className="w-10 h-10 border-[#3a4650] border-[1px] flex items-center justify-center select-none hover:cursor-default"
-                            >
-                                <div className={
-                                    `flex items-center justify-center z-10 
-                                    transition-transform bg-blue-500 w-full h-full 
-                                    ${(!value2.cleared && !value2.flaged) ? "hover:bg-blue-900" : ""} 
-                                    ${value2.cleared ? "duration-700 scale-0" : (value2.flaged ? "duration-150 scale-[0.8] bg-gray-500" : "scale-100")}`}
-                                >
-                                    {value2.flaged ? 
-                                        <div className="w-[80%] h-[80%]">
-                                            <Image src={flagImg} alt="Flag" draggable={false} />
-                                        </div> 
-                                    : ""}
-                                </div>
-                                <div className="absolute z-0 text-2xl">{value2.minesAround == 0 ? "" : value2.minesAround}</div>
-                            </div>
+                        {row.map((sig,index2)=>(
+                            <Cell key={`${index1} ${index2}`} 
+                                handleDig={()=>{ props.socket.emit("dig", props.roomId, index1, index2) }} 
+                                handleFlag={(e)=>{ e.preventDefault(); props.socket.emit("flag", props.roomId, index1, index2) }} 
+                                sig={sig} 
+                            />
                         ))}
                     </div>
                 ))}
